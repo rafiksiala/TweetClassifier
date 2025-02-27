677f936e8db9f6b78facec3ed313e35a68bd5449
rafiksiala <siala.rafik@gmail.com>
2025-02-27 15:01:48 +0100

    Streamlit

---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import requests
+
+# Configuration de l'API
+API_URL = "https://text-sentiment-app.azurewebsites.net/predict/"
+
+st.title("Analyse de Sentiment avec FastAPI et Streamlit")
+
+user_input = st.text_area("Entrez votre texte ici :")
+
+if st.button("Prédire le sentiment"):
+    if user_input:
+        response = requests.post(API_URL, json={"name": user_input})
+        if response.status_code == 200:
+            result = response.json()
+            st.write(f"Sentiment : **{result['sentiment']}**")
+            st.write(f"Confiance : **{result['confiance']:.2f}**")
+        else:
+            st.error("Erreur lors de la requête à l'API.")
+    else:
+        st.warning("Veuillez entrer un texte.")
