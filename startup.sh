3f5e61e72bcaf7adfa2a0ec759bb0750b8c3f428
rafiksiala <siala.rafik@gmail.com>
2025-02-27 16:16:56 +0100

    Streamlit

--- startup.sh
+++ startup.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-
-# Lancer FastAPI en arrière-plan
-gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app --bind 127.0.0.1:8000 &
-
-# Lancer Streamlit en arrière-plan
-streamlit run app.py --server.port 8501 --server.address 127.0.0.1 &
-
-# Démarrer NGINX
-nginx -c nginx.conf
+streamlit run app.py --server.port 8000 --server.address=0.0.0.0
