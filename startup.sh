a4c8ab1625769d9b591218a9f917b9ac0d788fcc
rafiksiala <siala.rafik@gmail.com>
2025-02-28 04:20:02 +0100

    Streamlit (Update startup.sh)

--- startup.sh
+++ startup.sh
@@ -4,4 +4,4 @@
 gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app --bind 0.0.0.0:8000 &
 
 # Lancer Streamlit sur un autre port (8501)
-streamlit run streamlit_app.py --server.port 8501 --server.address 0.0.0.0
+streamlit run app.py --server.port 8501 --server.address 0.0.0.0
