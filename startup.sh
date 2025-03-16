20570ffbe9bc9835693d1c1b62238c65dfe4bb39
rafiksiala <siala.rafik@gmail.com>
2025-03-16 14:32:11 +0100

    make API accessible

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Lancer FastAPI avec Gunicorn en arrière-plan sur le port 8001
-gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app --bind 127.0.0.1:8001 &
+gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app --bind 0.0.0.0:8001 &
 
 # Lancer Streamlit sur le port 8000 (port public)
 streamlit run app.py --server.port 8000 --server.address 0.0.0.0
